520df9640da367cc7a18c3c1697c98aa6c9ab3ea
Venya Brodetskiy <benjamincheg@gmail.com>
2022-10-01 16:05:16 +0300

    Store's CRUD: added delete

--- source/constants.ts
+++ source/constants.ts
@@ -3,11 +3,13 @@ export class SqlParameters {
     public static Id: string = "id";
 }
 export class Queries {
-    public static allStores: string = 
+    // Queries for Stores management
+    public static AllStores: string = 
         `SELECT stores.id, store_name, store_address, opening_date, scale
         FROM stores
-        INNER JOIN store_scale ON store_scale_id = store_scale.id`;
-    public static StoreById: string = "SELECT * FROM stores WHERE id = ?";
+        INNER JOIN store_scale ON store_scale_id = store_scale.id
+        WHERE status_id = ?`;
+    public static StoreById: string = "SELECT * FROM stores WHERE id = ? AND status_id = ?";
     public static StoreByTitle: string = "SELECT * FROM stores WHERE store_name LIKE ?";
     public static UpdateStoreById: string = `
         UPDATE stores 
@@ -18,15 +20,23 @@ export class Queries {
             update_date = ?, 
             update_user_id = ? 
         WHERE id = ? AND status_id = ?`;
-    public static AddNewStore: string = "EXEC sp_add_new_store ";
-    // @store_name NVARCHAR(50), @store_address NVARCHAR(50), @opening_date DATETIME, @store_scale NVARCHAR(50)
+    public static AddNewStore: string = `
+        INSERT stores 
+            (store_name, store_address, opening_date, store_scale_id, 
+            create_date, update_date,
+            create_user_id, update_user_id,
+            status_id)
+        VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )`;
+    public static DeleteStore: string = `
+        UPDATE stores
+        SET update_date = ?,
+            update_user_id = ?,
+            status_id = ?
+        WHERE id = ? AND status_id = ?`;
 
     public static SelectIdentity: string = "SELECT SCOPE_IDENTITY() AS id";
-   
-    
-    
-    // public static AddWhiteBoardType: string = "INSERT white_board_type (white_board_type) VALUES (?)";
-    // public static DeleteWhiteBoardTypeById: string = "UPDATE white_board_type SET update_date = ?, update_user = ?, status_id = ? WHERE id = ? AND status_id = ?";
+
+    // Queries for User management
     public static GetUserByLogin: string = "SELECT id, password, role_id FROM [user] WHERE login = ?";
     public static UpdateUserById: string = `
         UPDATE [user] 
--- source/routes/store.routes.ts
+++ source/routes/store.routes.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import storeController from '../controllers/store.controller';
-import employeeController from '../controllers/employee.controller';
 import middleware from '../middleware/auth.middleware';
 import { Role } from '../enums';
 const router = express.Router();
@@ -11,13 +10,6 @@ router.get('/by-title/:title', middleware.verifyToken([Role.Administrator, Role.
 
 router.put('/:id', middleware.verifyToken([Role.Administrator, Role.RegularUser]), storeController.updateStoreById);
 router.post('/', middleware.verifyToken([Role.Administrator, Role.RegularUser]), storeController.addNewStore);
-//router.put('/store', storeController.addNewStore);
-
-// router.post('/general/board-types', controller.addBoardType);
-// router.post('/general/board-types2', controller.addBoardType2);
-
-// router.delete('/general/board-types/:id', controller.deleteBoardTypeById);
-
-router.get('/employee', employeeController.getAllEmployees);
+router.delete('/:id', middleware.verifyToken([Role.Administrator, Role.RegularUser]), storeController.deleteStore)
 
 export default { router }; 
